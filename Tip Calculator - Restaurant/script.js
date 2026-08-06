46f92ac6dfe885811bd16a31a86964f30b9be895
mdshahriarfahim <msfahim71291@gmail.com>
2026-08-06 11:04:07 +0600

    add to the main calc function

--- Tip Calculator - Restaurant/script.js
+++ Tip Calculator - Restaurant/script.js
@@ -10,3 +10,14 @@ let bill = 0, tipPct =10,people= 1;
 const $= (id) => document.getElementById(id)
 const billE1 = $("bill"), peopleE1 = $("people");
 const tipE1 = $(tip), totelE1 = $("total"), eachE1 = $("each");
+
+//higher -order helper -main calc function
+
+function calc(){
+    const tip = percent(bill , tipPct);
+    const total = bill +tip;
+    const each = total/ Math.max(1 ,people);
+    tipE1.textContent ="TK" + round2(tip);
+    totalE1.textContent ="TK" + round2(total);
+    eachE1.textContent ="Tk" + round2(each);
+}

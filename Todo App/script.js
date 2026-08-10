b0c336c6bf679dba7f9f13fbd05fc81191c0c38b
mdshahriarfahim <msfahim71291@gmail.com>
2026-08-10 22:42:37 +0600

    create html,css and js file and update the code

---
+++ Todo App/script.js
@@ -0,0 +1,9 @@
+// Elements
+
+const input = document.getElementById("todoInput");
+const addbtn = document.getElementById("addbtn");
+const list = document.getElementById("todoList");
+const count = document.getElementById("count");
+
+// update counter
+

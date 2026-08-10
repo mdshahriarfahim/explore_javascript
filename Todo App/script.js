91cb5fee72cf06b82cfd9be1ed5ebb3b5276fae9
mdshahriarfahim <msfahim71291@gmail.com>
2026-08-10 23:03:12 +0600

    add to to todo function

--- Todo App/script.js
+++ Todo App/script.js
@@ -6,6 +6,16 @@ const list = document.getElementById("todoList");
 const count = document.getElementById("count");
 
 // update counter
-function updateCount{
-    
+function updateCount (){
+    const total = list.children.length;
+    count.innerText ="Total:" + total;
+}
+
+//Add todo
+function addTodo(){
+    const text ="input.value.trim()";
+    if (text === " "){
+        alert("Something write!");
+        return;
+    }
 }
